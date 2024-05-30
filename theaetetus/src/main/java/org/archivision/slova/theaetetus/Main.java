4e4a0d843f08335e8cd3962d6911e06dc1db21d2
Serwio YK <56473255+Serwios@users.noreply.github.com>
2024-05-30 23:59:26 +0300

    Translated first 10 lines.

---
+++ theaetetus/src/main/java/org/archivision/slova/theaetetus/Main.java
@@ -0,0 +1,22 @@
+package org.archivision.slova.theaetetus;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.Ukrainian;
+import org.languagetool.rules.RuleMatch;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        JLanguageTool langTool = new JLanguageTool(new Ukrainian());
+        List<RuleMatch> matches = langTool.check("Це тестовий текст на українській мові.");
+        for (RuleMatch match : matches) {
+            System.out.println("Potential error at characters " +
+                    match.getFromPos() + "-" + match.getToPos() + ": " +
+                    match.getMessage());
+            System.out.println("Suggested correction(s): " +
+                    match.getSuggestedReplacements());
+        }
+    }
+}
